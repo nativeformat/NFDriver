a46153ccddd8c6b55b7713433dd830722c38643a
David Rubinstein <drubinstein@users.noreply.github.com>
2018-07-30 15:17:11 -0400

    Documenting NFDriver interface.

--- include/NFDriver/NFDriver.h
+++ include/NFDriver/NFDriver.h
@@ -37,34 +37,109 @@
 namespace nativeformat {
 namespace driver {
 
-typedef enum { OutputTypeSoundCard, OutputTypeFile } OutputType;
+//! Enum that defines the desired output type
+/*! This enum will define the output destination. */
+typedef enum {
+  OutputTypeSoundCard, /*!< Output to hardware (the local sound card). */
+  OutputTypeFile       /* Output to a file. */
+} OutputType;
 
-#define NF_DRIVER_SAMPLE_BLOCK_SIZE 1024
-#define NF_DRIVER_SAMPLERATE 44100
+/*! Number of samples to process at a time */
+#define NF_DRIVER_SAMPLE_BLOCK_SIZE 1024 
+/*! The sample rate of the blocks to be sampled. In units of samples per second */
+#define NF_DRIVER_SAMPLERATE 44100 
+/*! Number of channels to output a time. 2 means we're outputting stereo */
 #define NF_DRIVER_CHANNELS 2
 
+/*!
+ * \brief Callback called when driver stutters.
+ *
+ * \param clientdata Client specific data that gets used by the callback.
+ */
 typedef void (*NF_STUTTER_CALLBACK)(void *clientdata);
+/*!
+ * \brief Callback called before rendering.
+ *
+ * \param clientdata Client specific data that gets used by the callback.
+ */
 typedef void (*NF_WILL_RENDER_CALLBACK)(void *clientdata);
+/*!
+ * \brief Callback called after rendering.
+ *
+ * \param clientdata Client specific data that gets used by the callback.
+ */
 typedef void (*NF_DID_RENDER_CALLBACK)(void *clientdata);
-typedef int (*NF_RENDER_CALLBACK)(void *clientdata, float *frames, int numberOfFrames);
-typedef void (*NF_ERROR_CALLBACK)(void *clientdata, const char *errorMessage, int errorCode);
+/*!
+ * \brief Callback that gathers frames to be output.
+ *
+ * \param clientdata Client specific data that gets used by the callback.
+ * \param frames Data that will be subsequently be output to the OutputType of choice.
+ * \param numberOfFrames Desired number of frames to store in frames.
+ * \return Number of frames that were stored in frames.
+ */
+typedef int (*NF_RENDER_CALLBACK)(void *clientdata, float *frames,
+                                  int numberOfFrames);
+/*! 
+ * \brief Callback called when NFDriver experiences an error.
+ * \param clientdata Client specific data that gets used by the callback.
+ * \param errorMessages Error message to print when this callback gets called.
+ * \param errorCode Unique number (preferably) that's associated with the error message.
+ */
+typedef void (*NF_ERROR_CALLBACK)(void *clientdata, const char *errorMessage,
+                                  int errorCode);
 
+/*! 
+ * \brief The version of this library.
+ *
+ * \return The version, for example 1.0 in use
+ */
 extern const char *version();
 
+/*!
+ * Interface used tracking state of the audio output.
+ */
 class NFDriver {
- public:
-  virtual bool isPlaying() const = 0;         // Thread-safe.
-  virtual void setPlaying(bool playing) = 0;  // Thread-safe.
+public:
+  /*!
+   * \brief Thread-safe function to check whether the driver is currently outputting samples.
+   * \return True if there are samples being output. False otherwise.
+   */
+  virtual bool isPlaying() const = 0;        // Thread-safe.
+  /*!
+   * \brief Thread-safe function to set if the driver should output samples.
+   * \param playing True to tell the driver to start outputting samples, False if not.
+   */
+  virtual void setPlaying(bool playing) = 0; // Thread-safe.
+  /*! \brief Destructor */
   virtual ~NFDriver(){};
 
 #if __ANDROID__
-  // Should be called only once per app life-cycle.
-  static void onAppLaunch(JNIEnv *env,
-                          jobject self,
-                          void *clientdata,
+  /*! 
+   * \brief Function to be called when using NFDriver with Android and app has just started.
+   * 
+   * This function is meant to be used with the Java Native Interface (JNI). It will be called when the application using NFDriver has just launched. This function should only be called ONCE per app life-cycle.
+   * \param env JNI Environment
+   * \param self The object calling this function
+   * \param clientdata Client specific data that gets used by the callback.
+   * \param errorCallback Function to call in case NFDriver errors.
+   */
+  static void onAppLaunch(JNIEnv *env, jobject self, void *clientdata,
                           NF_ERROR_CALLBACK errorCallback);
 #endif
 
+  /*!
+   * \brief Factory function to create an NFDriver for the appropriate device.
+   *
+   * \param clientdata Client specific data that gets used by the callback.
+   * \param stutter_callback Function to call if playback stutters.
+   * \param render_callback Function called when we have samples to output.
+   * \param error_callback Function called when the driver errors.
+   * \param will_render_callback Function called before render_callback.
+   * \param did_render_callback Function called after render_callback.
+   * \param outputType Desired output destination.
+   * \param output_destination Name of output destination if it is a named device, file etc.
+   * \return Instance of NFDriver.
+   */
   static NFDriver *createNFDriver(void *clientdata,
                                   NF_STUTTER_CALLBACK stutter_callback,
                                   NF_RENDER_CALLBACK render_callback,
